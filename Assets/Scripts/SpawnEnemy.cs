9c1fa6d8bf6dcc64ebbf83fb2161dcbb1f020532
Blu-ea <antoine.miguez@gmail.com>
2025-01-22 16:20:17 +0100

    Update: Enemy spawn in corner

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,30 +1,51 @@
-using System.Collections.Generic;
+using System.Collections;
 using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class SpawnEnemy : MonoBehaviour
 {
     
     // public GameObject[] enemyType;
 
-    [SerializedDictionary("Enemy Type", "Spawn Timer")]
-    public SerializedDictionary<GameObject, float> spawnTime;
-    private bool[] _spawnable; 
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [FormerlySerializedAs("spawnTime")] [SerializedDictionary("Enemy Type", "Spawn Timer +- 3s")]
+    public SerializedDictionary<GameObject, float> allEnemy;
+
+    private Vector3 _spawnRange;
+    private IEnumerator SpawnEnemyCoroutine(GameObject enemy, float spawnTime)
     {
-        // for (int i = 0; i < enemyType.Length; i++)
-        // {
-            // spawnTime.Add(gameObject, Random.Range(4f * i, 6f * i));
-            // _spawnable[i] = false;
-        // }
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(spawnTime - 3f, spawnTime + 3f));
+            Instantiate(enemy, _spawnRange, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_spawnRange, 1);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        foreach (var enemy in allEnemy)
+        {
+            StartCoroutine(SpawnEnemyCoroutine(enemy.Key, enemy.Value));
+        }
+    }
+
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        // var isNeggative = Random.Range(0, 2) * 2 - 1;
+        _spawnRange = new Vector3(
+            Random.Range(5, 10) * (Random.Range(0, 2) * 2 - 1) + transform.position.x,
+            Random.Range(5, 10) * (Random.Range(0, 2) * 2 - 1) + transform.position.y,
+            0F);
+
     }
 }
